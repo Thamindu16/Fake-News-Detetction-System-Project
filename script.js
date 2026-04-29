9e09316ad29929b105384c266b56e71770053a2b
Thamindu Kavinda <dinujayatk@gmail.com>
2026-04-29 20:08:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
         resetUI();
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/predict", {
+            const response = await fetch("https://fake-news-api-5lv4.onrender.com", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
